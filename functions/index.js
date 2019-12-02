0badc73dfc29bafac5474c4890ae3d9872ecc91c
allansendagi <sendagiallan87@gmail.com>
2019-12-02 06:18:27 +0400

    adding signin route

--- functions/index.js
+++ functions/index.js
@@ -53,15 +53,43 @@ app.get('/mails', (request, response) => {
 	 })
 	 .catch(err => console.error(err));
 })
+const FBAuth = (request, response, next) => {
+	let idToken;
+	if (request.headers.authorization && request.headers.authorization.startWith('Bearer ')) {
+		idToken = request.headers.authorization = split('Bearer ')[1];
+	} else {
+		console.error('No token found')
+		return response.status(403).json({ error: 'unathorized'})
+	}
 
+	admin.auth().verifyIdToken(idToken)
+	.then(decodedToken => {
+		request.user = decodedToken;
+		console.log(decodedToken);
+		return db.collection('users')
+		  .where('userId', '==', request.user.uid)
+		  .limit(1)
+		  .get();
+	})
+	.then(data => {
+		request.user.handle = data.docs[0].data().handle;
+		return next();
+	})
+	.catch(err => {
+		console.error('Error while verifying token', err);
+		return response.status(403).json(err);
+	})
+}
 
-
-
-app.post('/update', (request, response) => {
+//post update
+app.post('/update', FBAuth, (request, response) => {
+	if (request.body.body.trim()==='') {
+		return response.status(400).json({ body: "Body must not be empty"});
+	}
 	
 	const newMail = {
 		body: request.body.body,
-		userHandle: request.body.userHandle,
+		userHandle: request.user.handle,
 		createdAt: new Date().toISOString()
 	};
 	     db
@@ -75,6 +103,16 @@ app.post('/update', (request, response) => {
 	     	console.error(err);
 	     })
 });
+const isEmail = (email) => {
+	const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (email.match(regEx)) return true;
+	else return false;
+}
+
+const isEmpty = (string) => {
+	if (string.trim() === '') return true;
+	else return false;
+}
 //SignUproute
 
 app.post('/signup', (request, response)=> {
@@ -84,8 +122,25 @@ app.post('/signup', (request, response)=> {
 	    confirmPassword: request.body.confirmPassword,
 	    handle: request.body.handle,
 	}
+	let errors = {};
+
+	if (isEmpty(newUser.email)) {
+		errors.email = 'Must not be empty'
+	} else if (!isEmail(newUser.email)){
+		errors.email = 'Must be a valid email address'
+	}
+
+	if (isEmpty(newUser.password)) errors.password = 'Must not be empty'
+	if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+	if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+	if (Object.keys(errors).length > 0) return response.status(400).json(errors);
+
+
+	
 	//validate data
-	let token, userId; 
+	let token;
+	let userId;
 
 	db.doc(`/users/${newUser.handle}`).get()
 	   .then(doc => {
@@ -123,6 +178,40 @@ app.post('/signup', (request, response)=> {
 	   	   }
 	   	})
 });
+
+app.post('/login', (request, response) => {
+	const user = {
+		email: request.body.email,
+		password: request.body.password
+	};
+
+	let errors = {};
+
+	if (isEmpty(user.email)) errors.email = "Must not be empty";
+	if (isEmpty(user.password)) errors.password = "Must not be empty";
+
+	if (Object.keys(errors).length > 0) return response.status(400).json(errors);
+
+	firebase
+	 .auth()
+	 .signInWithEmailAndPassword(user.email, user.password)
+	 .then(data => {
+	 	return data.user.getIdToken();
+	 })
+	 .then(token => {
+	 	return response.json({token});
+	 })
+	 .catch(err => {
+	 	console.error(err);
+	 	if (err.code === 'auth/wrong-password'){
+	 		return response
+	 		.status(403)
+	 		.json({ general: 'Wrong credentials, please try again'});
+	 	} else 
+	 	return response.status(500).json({ error: err.code });
+	 })
+
+})
 
 exports.api = functions.https.onRequest(app);
 
